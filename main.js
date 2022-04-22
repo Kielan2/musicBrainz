f252ab4b9bc8ab27c87c59dd98470007a47bc6d5
Kielan2 <kielananderson@gmail.com>
2022-04-22 17:47:12 -0600

    codewar

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
-const APIController = (function() {
+// const APIController = (function() {
   
 
-  const clientId = ''
-  const clientSecret = ''
+//   const clientId = ''
+//   const clientSecret = ''
 
-  const _getTOken = async () => {
+//   const _getTOken = async () => {
      
-    const result = await fetch('https://accounts.spotify.com/api/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type' : 'application/x-www-form-urlencoded',
-        'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
-      },
-      body: 'grant_type=client_credentials'
-    })
+//     const result = await fetch('https://accounts.spotify.com/api/token', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type' : 'application/x-www-form-urlencoded',
+//         'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
+//       },
+//       body: 'grant_type=client_credentials'
+//     })
 
-    const data = await result.json()
-    return data.access_token
-  }
+//     const data = await result.json()
+//     return data.access_token
+//   }
 
-})()
+// })()
+
+function giveArtist(){
+  let ourArtist = document.querySelector('#artistSearch').value
+
+fetch(`http://open.spotify.com/track/6rqhFgbbKwnb9MLmUQDhG6`)
+  .then(response => response.json())
+  .then(data => {
+      console.log(data)
+  })
+}
